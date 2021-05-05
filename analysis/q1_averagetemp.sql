4bbb82c9abab36e82ad0022c5c31c15953cecfd0
andrew.hakimian1@baruchmail.cuny.edu <andrew.hakimian1@baruchmail.cuny.edu>
2021-05-05 01:56:14 +0000

    Updated fact table

--- analysis/q1_averagetemp.sql
+++ analysis/q1_averagetemp.sql
@@ -1,6 +1,6 @@
 select 
-entry_month, 
-TAVG as average_temp
+NYC_weather_dim.entry_month, 
+facts_table.avg_temp as average_temp
 from cloud-collective.dbt_cloudcollective.facts_table, cloud-collective.dbt_cloudcollective.NYC_weather_dim
 where entry_month = 'January' OR entry_month = 'February' OR entry_month = 'March'
-group by entry_month
+group by NYC_weather_dim.entry_month, facts_table.avg_temp
